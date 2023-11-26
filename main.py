926a7e4a7ecd9c61c13cd794d3856e02646927ef
Kha Nguyen <72005690+ndkcuber@users.noreply.github.com>
2023-11-26 12:31:25 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
-#ndkcuber is the bezt :)))
+#dongkha is the bezt :)))
 
 import pyautogui
 import random
 import subprocess
 import datetime
 import time
+import pyperclip
 
 def spammer():
-	while True:
-		activate_chat = pyautogui.locateOnScreen('type_message_here.png')
-		if activate_chat != None:
-			pyautogui.click(activate_chat)
-			text_file = open('text.txt','r')
-			lines = text_file.readlines()
-			for line in lines:
-				pyautogui.typewrite(line)
-				pyautogui.press('enter')
-			break
-		else:
-			print('cannot find message field')
-
+    while True:
+        activate_chat = pyautogui.locateOnScreen('type_message_here.png')
+        if activate_chat is not None:
+            pyautogui.click(activate_chat)
+            with open('text.txt', 'r', encoding='utf-8') as text_file:
+                lines = text_file.readlines()
+                for line in lines:
+                    pyperclip.copy(line.strip())  # strip to remove newline characters
+                    pyautogui.keyDown('ctrl')
+                    pyautogui.press('v')
+                    pyautogui.keyUp('ctrl')
+                    pyautogui.press('enter')
+            break
+        else:
+            print('cannot find message field!')
 
 spammer()
+
 #main client
 #type command here to run the client
